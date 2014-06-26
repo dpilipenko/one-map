a5b59569643a328a6d8d26f77bab17a593a45fb4
Dan Padgett <dan.padgett1@gmail.com>
2014-06-26 19:50:54 -0400

    adding room, desk, pin tables.

---
+++ one-map/grails-app/domain/com/rosetta/onemap/pintypes/Desk.groovy
@@ -0,0 +1,13 @@
+package com.rosetta.onemap.pintypes
+
+import com.rosetta.onemap.Pin
+import com.rosetta.onemap.User
+
+class Desk extends Pin {
+
+	User user
+	
+    static constraints = {
+		
+    }
+}
