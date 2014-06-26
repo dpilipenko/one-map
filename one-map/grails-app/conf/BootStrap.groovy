9809ddb8c115a046c0ba5a7d8e07d20c0bf82ce5
Dan Padgett <dan.padgett1@gmail.com>
2014-06-26 17:28:38 -0400

    fix merge

--- one-map/grails-app/conf/BootStrap.groovy
+++ one-map/grails-app/conf/BootStrap.groovy
@@ -3,9 +3,6 @@ import com.rosetta.onemap.User
 class BootStrap {
 
     def init = { servletContext ->
-		User dan = new User();
-		dan.firstName = "Dan";
-		dan.save(flush: true);
     }
     def destroy = {
     }
