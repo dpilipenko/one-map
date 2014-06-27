6ce7e1ae8594f77b5f60e988d0c96ad470358347
Dan Padgett <dan.padgett1@gmail.com>
2014-06-27 03:21:10 -0400

    update search to return json response

--- one-map/grails-app/domain/com/rosetta/onemap/pintypes/Room.groovy
+++ one-map/grails-app/domain/com/rosetta/onemap/pintypes/Room.groovy
@@ -1,10 +1,13 @@
 package com.rosetta.onemap.pintypes
 
+import com.rosetta.onemap.Office
 import com.rosetta.onemap.Pin
 import com.rosetta.onemap.User
 
 class Room extends Pin {
 
+	Office office
+	
     static constraints = {
     
 	}
