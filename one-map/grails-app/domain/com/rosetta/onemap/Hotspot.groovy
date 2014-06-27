b75f1b63a827998d26bc3f18538b83a89b0496a2
Dmitriy Pilipenko <dmitriy.pilipenko@gmail.com>
2014-06-26 23:41:48 -0400

    Roommania

--- one-map/grails-app/domain/com/rosetta/onemap/Hotspot.groovy
+++ one-map/grails-app/domain/com/rosetta/onemap/Hotspot.groovy
@@ -3,9 +3,11 @@ package com.rosetta.onemap
 class Hotspot {
 
 	String floor
+	String type
 	String polygon
 	
     static constraints = {
 		polygon blank: false
+		type inList: ["desk", "room"]
     }
 }
