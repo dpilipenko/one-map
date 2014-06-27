399150ac88342d6ce1cc985a2f78bbc51ab4f774
elizabethsjudd <elizabethsjudd@gmail.com>
2014-06-27 02:29:02 -0400

    fixed image sizes

--- one-map/web-app/js/main.js
+++ one-map/web-app/js/main.js
@@ -56,31 +56,6 @@ var phoneSlideshow = (function() {
 })();
 
 var RosettaMap = { 
-  utilities: {
-    createCookie: function(name,value,days) {
-      if (days) {
-        var date = new Date();
-        date.setTime(date.getTime()+(days*24*60*60*1000));
-        var expires = "; expires="+date.toGMTString();
-      }
-      else var expires = "";
-      document.cookie = name+"="+value+expires+"; path=/";
-    },
-    readCookie: function(name) {
-      var nameEQ = name + "=";
-      var ca = document.cookie.split(';');
-      for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-      }
-      return null;
-    },
-    eraseCookie: function(name) {
-      var scope = this;
-      scope.createCookie(name,"",-1);
-    }
-  }, 
   mapSetup: {
     zoomMultiplier: 0.1,
     stage: null,
@@ -98,7 +73,41 @@ var RosettaMap = {
     hotspotHoverOpacity: 1,
 
     initFloorplan: function (id, imgSrc, floorNumber) {
-      var scope = this;
+    	var scope = this,
+      		isHorizontal = false;
+    	
+    	console.log(typeof floorNumber);
+    	console.log(floorNumber);
+    	switch(floorNumber) {
+	    	case "11":
+	    		scope.stageWidth = 550; //
+	    	    scope.stageHeight = 526;
+	    		break;
+	    	case "12":
+	    		scope.stageWidth = 554;
+	    	    scope.stageHeight = 528;
+	    		break;
+	    	case "13":
+	    		scope.stageWidth = 552;
+	    	    scope.stageHeight = 528;
+	    		break;
+	    	case "14":
+	    		scope.stageWidth = 521; //
+	    	    scope.stageHeight = 501;
+	    		break;
+	    	case "15":
+	    		scope.stageWidth = 550; //
+	    	    scope.stageHeight = 528;
+	    		break;
+	    	case "17":
+	    		scope.stageWidth = 520; //
+	    	    scope.stageHeight = 523;
+	    		break;
+	    	default:
+	    		alert('not a floor');
+	    		break;
+    	}
+      
       
       scope.stageScaleX = $('.showthisfloor').width()/scope.stageWidth;
       scope.stageScaleY = $('.showthisfloor').height()/scope.stageHeight;
@@ -107,6 +116,7 @@ var RosettaMap = {
       // is container tall or wide
       if(scope.stageScaleX > scope.stageScaleY) {
         scope.stageScale = scope.stageScaleY;
+        isHorizontal = true;
       } else {
         scope.stageScale = scope.stageScaleX;
       }
@@ -114,10 +124,13 @@ var RosettaMap = {
       // set up kinetic stages and layers
       var stage = new Kinetic.Stage({
         container: id,
-        width: scope.stageWidth * scope.stageScale,
-        height: scope.stageHeight * scope.stageScale,
-        draggable: false
+        width: $('.showthisfloor').width(), //scope.stageWidth * scope.stageScale,
+        height: $('.showthisfloor').height(), //scope.stageHeight * scope.stageScale,
+        draggable: false,
+        fill: "#e2e2e2"
       });
+	  //stage.style.background = "#e2e2e2";
+
       var layer = new Kinetic.Layer({
         x: 0,
         y: 0,
@@ -142,14 +155,24 @@ var RosettaMap = {
       var floorplanObj = new Image();
       floorplanObj.src = 'images/'+ imgSrc + '.png';
       
+      var imageWidth = scope.stageWidth * scope.stageScale,
+      	imageHeight = scope.stageHeight * scope.stageScale,
+      	imageX = 0,
+      	imageY = 0;
+      // image centering
+      if(isHorizontal) {
+    	  imageX = (stage.width() - imageWidth)/2;
+      } else {
+    	  imageY = (stage.height() - imageHeight)/2;
+      }
       // display canvas when image is loaded
       floorplanObj.onload = function() {
         var floorplan = new Kinetic.Image({
-          x: 0,
-          y: 0,
+          x: imageX,
+          y: imageY,
           image: floorplanObj,
-          width: stage.width(),
-          height: stage.height(),
+          width: imageWidth,
+          height: imageHeight,
         });
 
         // add the image to the layer
@@ -165,10 +188,10 @@ var RosettaMap = {
             	for(var key in data) {
                     //create hotspot
                     var hotspotPath = new Kinetic.Path({
-                      x: 0,
-                      y: 0,
-                      width: stage.width(),
-                      height: stage.height(),
+                      x: imageX,
+                      y: imageY,
+                      width: imageWidth,
+                      height: imageHeight,
                       id: key,
                       scale: {x: scope.stageScale, y: scope.stageScale},
                       data: data[key],
