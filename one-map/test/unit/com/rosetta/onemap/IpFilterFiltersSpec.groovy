4a8e6661ca4e29289032acedf95b8658b943359f
Dmitriy Pilipenko <dmitriy.pilipenko@rosetta.com>
2014-09-07 15:02:31 -0400

    IP Filter

---
+++ one-map/test/unit/com/rosetta/onemap/IpFilterFiltersSpec.groovy
@@ -0,0 +1,19 @@
+package com.rosetta.onemap
+
+import com.rosetta.onemap.IpFilters;
+
+import grails.test.mixin.Mock
+import spock.lang.Specification
+
+@Mock(IpFilters)
+class IpFilterFiltersSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
