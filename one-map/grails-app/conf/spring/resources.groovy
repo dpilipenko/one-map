bd32da3060911dad94ae779b767a259875bb0aaf
Dmitriy Pilipenko <dmitriy.pilipenko@gmail.com>
2015-02-14 13:26:32 -0500

    fix project structure

--- one-map/grails-app/conf/spring/resources.groovy
+++
@@ -1,8 +0,0 @@
-import com.rosetta.onemap.ldap.LDAPUserMapper;
-
-// Place your Spring DSL code here
-beans = {
-	ldapUserDetailsMapper(LDAPUserMapper) {
-		// bean attributes
-	}
-}
--- one-map/grails-app/domain/com/rosetta/onemap/Office.groovy
+++
@@ -1,8 +0,0 @@
-package com.rosetta.onemap
-
-class Office {
-	String name
-    static constraints = {
-		name(blank: false)
-    }
-}
